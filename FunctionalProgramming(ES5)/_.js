91076478d38ff53fd7cb9b0adad1dcc7b375d812
johny <69754601+jskjw157@users.noreply.github.com>
2021-08-10 16:32:12 +0900

    reduce and pipe and go function

--- FunctionalProgramming(ES5)/_.js
+++ FunctionalProgramming(ES5)/_.js
@@ -1,4 +1,14 @@
 
+function _curryr(fn) {
+    return function(a,b) {
+        return arguments.length == 2 ?  fn(a,b) : function(b){return fn(b,a);};
+    } 
+}
+
+var _get = _curryr( function(obj, key) {
+    return obj == null ? undefined : obj[key] ;
+});
+
 // each가 적용된 filter
 function _filter(list, predi) {
     var new_list = [];
@@ -24,3 +34,38 @@ function _each(list, iter) {
     }
     return list;
 }
+
+function _rest(list, num) {
+    return slice.call(list, num || 1); // array가 아닌 array_like이여도 call함수로 사용할 수 있게 해줌
+  } // 인자로 받은 num값의 수만큼 배열의 값을 제거 후 새로운 배열로 리턴
+  
+  
+  
+function _reduce(list, iter, memo) {
+    if (arguments.length == 2) {// 받은 인자가 2개 일 경우  초기값(memo)에 값을 대입해주고, 그값을 배열에서 제거
+        memo = list[0];
+        list = _rest(list);
+    }
+    _each(list, function (val) {
+        memo = iter(memo, val);
+    });
+    return memo;
+}
+
+function _pipe() {
+    var fns = arguments;
+    return function (arg) {
+        return _reduce(fns, function (arg, fn) {
+            return fn(arg);
+        }, arg)
+    }
+}
+
+function _go(arg) {
+    var fns = _rest(arguments);
+    return _pipe.apply(null, fns)(arg);
+}
+
+// map과 filter에 curryr함수 적용
+var _map = _curryr(_map),
+    _filter = _curryr(_filter);
